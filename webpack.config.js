83f0cf1ca248350721c7358ca895798cd58108b8
Karm1110 <ph.mujalli@gmail.com>
2025-08-28 17:39:21 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,25 @@
 const twilight = require('@salla.sa/twilight');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
-module.exports = {
-  entry: {
-    app: './src/assets/js/app.js',
-    home: './src/assets/js/home.js',
-    product: './src/assets/js/product.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'mini-css-extract-plugin/loader',
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
-        ],
-      },
+twilight.webpack(
+  {
+    entry: {
+      app: './src/assets/js/app.js',
+      home: './src/assets/js/home.js',
+      product: './src/assets/js/product.js',
+      'main-menu': './src/assets/js/partials/main-menu.js',
+      'product-card': './src/assets/js/partials/product-card.js',
+      'wishlist-card': './src/assets/js/partials/wishlist-card.js',
+      testimonials: './src/assets/js/testimonials.js',
+    },
+    output: {
+      path: path.resolve(__dirname, 'public', 'dist'),
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'app.css',
+      }),
     ],
-  },
-  plugins: [
-    new twilight.MiniCssExtractPlugin({
-      filename: 'app.css',
-    }),
-  ],
-};
+  }
+);
